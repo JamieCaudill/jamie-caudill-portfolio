8798a8c0151e28a5e0249dc1039cb7baffd49c9e
Jamie Caudill <j.caudill7177@gmail.com>
2023-07-23 15:38:35 -0700

    Initial commit

---
+++ src/Intro/Intro.js
@@ -0,0 +1,9 @@
+// Intro.js
+
+export default function Intro() {
+  return (
+    <div className="intro">
+      <h1 className="intro__text">Hi, I'm Jamie. I design & build user interfaces.</h1>
+    </div>
+  )
+}
