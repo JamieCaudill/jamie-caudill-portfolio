d83d81c277c1203a6a02793dda043290e2de8925
Jamie Caudill <j.caudill7177@gmail.com>
2023-08-11 10:28:54 -0700

    feat: Scroll to project details

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import './Projects.scss';
 
 import ProjectDetails from '../ProjectDetails/ProjectDetails';
 import data from '../../data/project-data';
+import { Link } from 'react-scroll';
 
 import { useState } from 'react';
 
@@ -25,19 +26,19 @@ const Projects = () => {
       <h1>Projects</h1>
         <div className="projects__item-container">
           <div className="projects__item">
-            <img className="projects__item-image" id="1" onClick={handleClick} src={require('../../images/humor-me.png')} alt="project" />
+            <Link to="project-details" spy={true} smooth={true} offset={0} duration={500}><img className="projects__item-image" id="1" onClick={handleClick} src={require('../../images/humor-me.png')} alt="project" /></Link>
             <h3>Humor Me</h3>
           </div>
           <div className="projects__item">
-            <img className="projects__item-image" id="2" onClick={handleClick} src={require('../../images/zen-tacos.png')} alt="project" />
+            <Link to="project-details" spy={true} smooth={true} offset={0} duration={500}><img className="projects__item-image" id="2" onClick={handleClick} src={require('../../images/zen-tacos.png')} alt="project" /></Link>
             <h3>Zen Tacos</h3>
           </div>
           <div className="projects__item">
-            <img className="projects__item-image" id="3" onClick={handleClick} src={require('../../images/overlook.png')} alt="project" />
+            <Link to="project-details" spy={true} smooth={true} offset={0} duration={500}><img className="projects__item-image" id="3" onClick={handleClick} src={require('../../images/overlook.png')} alt="project" /></Link>
             <h3>Overlook</h3>
           </div>
           <div className="projects__item">
-            <img className="projects__item-image" id="4" onClick={handleClick} src={require('../../images/whats-cookin.png')} alt="project" />
+            <Link to="project-details" spy={true} smooth={true} offset={0} duration={500}><img className="projects__item-image" id="4" onClick={handleClick} src={require('../../images/whats-cookin.png')} alt="project" /></Link>
             <h3>What's Cookin'</h3>
           </div>
         </div>
