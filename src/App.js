f0d12d679f51ea78f43615a62c9c16375d576b12
Jamie Caudill <j.caudill7177@gmail.com>
2023-08-11 11:15:03 -0700

    feat: Add footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar/Navbar';
 import Projects from './components/Projects/Projects';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 import { gsap } from 'gsap';
 
 
@@ -20,6 +21,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+// Footer.js //
+
+import './Footer.scss' 
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__container">
+        <h3>Built with React</h3>
+        <a href="https://github.com/JamieCaudill/jamie-caudill-portfolio" target="_blank" rel="noreferrer">Repo</a>
+        <p>© 2023 Jamie Caudill</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
