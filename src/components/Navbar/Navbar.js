0658220487593b40fc5e8977654e69dca536b919
Jamie Caudill <j.caudill7177@gmail.com>
2023-08-10 20:19:08 -0700

    feat: 550px breakpoints

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,29 +13,29 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar__container">
-      <div className="navbar__logo">
-        <img className="navbar__logo-image" src={require('../../images/logo3.png')} alt="logo" />
+        <div className="navbar__logo">
+          <img className="navbar__logo-image" src={require('../../images/logo3.png')} alt="logo" />
+        </div>
+        <nav className="navbar__items__container">
+          <ul className="navbar__items">
+            {/* <li className="navbar__item">
+              <Link to="intro" spy={true} smooth={true} offset={0} duration={500}>Home</Link>
+            </li> */}
+            <li className="navbar__item">
+              <Link to="projects" spy={true} smooth={true} offset={0} duration={500}>Projects</Link>
+            </li>
+            <li className="navbar__item">
+              <Link to="about" spy={true} smooth={true} offset={0} duration={500}>About</Link>
+            </li>
+            <li className="navbar__item">
+              <Link to="contact" spy={true} smooth={true} offset={0} duration={500}>Contact</Link>
+            </li>
+            <li className="navbar__item">
+              <button onClick={openResume}>Resume</button>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav className="navbar__items__container">
-        <ul className="navbar__items">
-          {/* <li className="navbar__item">
-            <Link to="intro" spy={true} smooth={true} offset={0} duration={500}>Home</Link>
-          </li> */}
-          <li className="navbar__item">
-            <Link to="projects" spy={true} smooth={true} offset={0} duration={500}>Projects</Link>
-          </li>
-          <li className="navbar__item">
-            <Link to="about" spy={true} smooth={true} offset={0} duration={500}>About</Link>
-          </li>
-          <li className="navbar__item">
-            <Link to="contact" spy={true} smooth={true} offset={0} duration={500}>Contact</Link>
-          </li>
-          <li className="navbar__item">
-            <button onClick={openResume}>Resume</button>
-          </li>
-        </ul>
-      </nav>
-    </div>
     </div>
   )
 }
